b485ece94db2d0568a9b718228416227b37dc16d
0djent <58089540+0djent@users.noreply.github.com>
2019-11-25 02:44:16 -0800

    nothing working

--- NotationTasks/Note.cs
+++ NotationTasks/Note.cs
@@ -10,24 +10,24 @@ namespace NotationTasks
         private bool needInit = true;
         private double[] freqTable = new double[88];
         private char[] noteNameTable = new char[7] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
-        private string[] noteNameTableSharps = new string[12] {"a ", "a#", "b ", "c ", "c#", "d ", "d#", "e ", "f ", "f#",  "g ", "g#" };
-        private string[] noteNameTableBemol = new string[12] {"a", "bb", "b", "c", "db", "d", "eb", "e", "f", "gb", "g", "ab" };
-        private string[] accidentalStringTable = new string[] { "bb", "b_", "__" ,"#_", "##" };
+        private string[] noteNameTableSharp = new string[12] {"a", "a", "b", "c", "c", "d", "d", "e", "f", "f",  "g", "g" };
+        private string[] noteNameTableFlat = new string[12] {"a", "b", "b", "c", "d", "d", "e", "e", "f", "g", "g", "a" };
+        private string[] accidentalsStringTable = new string[] { "bb", "b_", "__" ,"#_", "##" };
 
         // ab_4 a__4
 
         public void Init()
         {
             needInit = false;
-            for (int i=1; i<freqTable.Length; i++)
+            for (int i=0; i<freqTable.Length; i++)
             {
                 if (accuracy > 6)
                 {
-                    freqTable[i] = Math.Pow(2, ((i - referenceNote + 1) / 12)) * referenceNoteFreq;
+                    freqTable[i] = Math.Pow(2, ((i - referenceNote ) / 12)) * referenceNoteFreq;
                 }
                 else
                 {
-                    freqTable[i] = Math.Round(Math.Pow(2, ((i - referenceNote + 1) / 12)) * referenceNoteFreq, accuracy);
+                    freqTable[i] = Math.Round(Math.Pow(2, ((i - referenceNote ) / 12)) * referenceNoteFreq, accuracy);
                 }
             }
             
@@ -36,12 +36,56 @@ namespace NotationTasks
         public void ShowFreqTable()
         {
             if (needInit == true) Init();
-            for (int i = 0; i < freqTable.Length; i++)
+            for (int i = 1; i < freqTable.Length; i++)
             {
-                Console.WriteLine("freqTable [" + i + "] ["+GetNoteName(i,0)+"] = " + freqTable[i]+" Hz");
+                Console.WriteLine("freqTable [" + i + "] ["+GetNoteName(i)+"] = " + freqTable[i]+" Hz");
             }
         }
 
+        public string GetNoteName(int noteNumber)
+        {
+            noteNumber--;
+            if (needInit == true) Init();
+            int noteNumberInOctave = noteNumber % 12;
+            string noteName = noteNameTableSharp[noteNumberInOctave];
+            if (noteNumberInOctave == 1 | noteNumberInOctave == 4 | noteNumberInOctave == 6 | noteNumberInOctave == 9 | noteNumberInOctave == 11)
+            {
+                noteName += "#_";
+            }
+            else
+            {
+                noteName += "__";
+            }
+            return (noteName + (noteNumber / 12));
+        }
+
+        public int GetNoteNumber(string noteString)
+        {
+            for (int i = 0; i <= noteNameTable.Length; i++)
+            {
+                if (noteString[0] == noteNameTable[i])
+                {
+                    for (int x = 1; x < accidentalsStringTable.Length; x++)
+                    {
+                        if (noteString.Substring(1, 2) == accidentalsStringTable[x])
+                        {
+                            int accidentalInt = x - 2;
+                            return (i + 12 * Convert.ToInt32(noteString[3]) + accidentalInt);
+                        }
+                    }
+                    return (i + 12 * Convert.ToInt32(noteString[3]));
+                }
+            }
+            return 0;
+        }
+        public int GetNoteNumber(char note, int accidental, int octave)
+        {
+            string noteName = note.ToString() + accidentalsStringTable[accidental + 2] + octave;
+            return GetNoteNumber(noteName);
+        }
+
+
+        /*
         public int GetNoteNumber(char note, string accidental, int octave)
         {
             for (int i = 0; i<=noteNameTable.Length; i++)
@@ -117,7 +161,7 @@ namespace NotationTasks
         public int Compare(string note1, string note2)
         {
             return (StringToNum(note2) - StringToNum(note2));
-        }
+        }*/
 
     }
 }
--- NotationTasks/Test.cs
+++ NotationTasks/Test.cs
@@ -8,7 +8,12 @@ namespace NotationTasks
         {
             Note NOTE = new Note();
             NOTE.ShowFreqTable();
-            Console.WriteLine(NOTE.GetNoteName(48, 0) + " " + NOTE.GetFreq('a', 2, 4) + " " + NOTE.GetNoteNumber('a', 0, 4)+" "+ NOTE.StringToNum("a004"));
+            for (int i=48; i<60; i++)
+            {
+                Console.WriteLine(i + " " + NOTE.GetNoteName(i) + " " + NOTE.GetNoteNumber(NOTE.GetNoteName(i))) ;
+            }
+            Console.WriteLine(NOTE.GetNoteNumber('a', 1, 4));
+            //Console.WriteLine(NOTE.GetNoteName(48, 0) + " " + NOTE.GetFreq('a', 2, 4) + " " + NOTE.GetNoteNumber('a', 0, 4)+" "+ NOTE.StringToNum("a004"));
         }
     }
 }
