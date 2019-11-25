66b432aef199f5590257590486c32423c44255bf
0djent <58089540+0djent@users.noreply.github.com>
2019-11-25 09:29:47 -0800

    Update Note.cs

--- NotationTasks/Note.cs
+++ NotationTasks/Note.cs
@@ -2,11 +2,22 @@ namespace NotationTasks
 {
     class Note
     {
-        public int noteNumber;
-        public double noteFreq;
-        public int noteDuration;
-        public int noteTuplet;
-        public int noteStart;
-        public int noteArticulation;
+        private int pitch;
+        private double freq;
+        private int start;
+        private int duration;
+        private int tuplet;
+        private int articulation;
+
+        public int Pitch { get => pitch; set => pitch = value; }
+        public double Freq { get => freq; set => freq = value; }
+        public int Start { get => start; set => start = value; }
+        public int Duration { get => duration; set => duration = value; }
+        public int Tuplet { get => tuplet; set => tuplet = value; }
+        public int Articulation { get => articulation; set => articulation = value; }
+        public void Set(int setpitch, int start, int duration, int tuplet)
+        {
+            Pitch()=setpitch;
+        }
     }
 }
